0d72384cbf1d34466beba6857a9de09dc5afd92a
Iakov Volf <jakoff@gmail.com>
2015-05-27 21:00:38 +0300

    New class WhatWorks

--- src/test/java/com/telran/SampleTestNgTest.java
+++ src/test/java/com/telran/SampleTestNgTest.java
@@ -1,23 +1,19 @@
 package com.telran;
 
-import org.openqa.selenium.support.PageFactory;
-
-import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
-
 import com.telran.pages.HomePage;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 
 public class SampleTestNgTest extends TestNgTestBase {
 
   private HomePage homepage;
 
-  @BeforeMethod
+  // @BeforeMethod
   public void initPageObjects() {
     homepage = PageFactory.initElements(driver, HomePage.class);
   }
 
-  @Test
+  //@Test
   public void testHomePageHasAHeader() {
     driver.get(baseUrl);
     Assert.assertFalse("".equals(homepage.header.getText()));
---
+++ src/test/java/com/telran/pages/WhatsWorksOnMainPage.java
@@ -0,0 +1,68 @@
+package com.telran.pages;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+/**
+ * Created by Iakov Volf 27.05.15.
+ */
+public class WhatsWorksOnMainPage extends Page {
+    //Category Symptom buttons
+    @FindBy(xpath = "//div[@class='btn-group']/button[contains(text(),'Therapy')]")
+    WebElement therapyButton;
+
+    @FindBy(xpath = "//div[@class='btn-group']/button[contains(text(),'Equipment')]")
+    WebElement equipmentButton;
+
+    @FindBy(xpath = "//div[@class='btn-group']/button[contains(text(),'Nutrition')]")
+    WebElement nutritionButton;
+
+    @FindBy(xpath = "//div[@class='btn-group']/button[contains(text(),'Exercises')]")
+    WebElement exercisesButton;
+
+    @FindBy(xpath = "//div[@class='btn-group']/button[contains(text(),'Alternative')]")
+    WebElement alternativeButton;
+
+    @FindBy(xpath = "//div[@class='btn-group']/button[contains(text(),'Other')]")
+    WebElement otherButton;
+    //Dropdown list
+    @FindBy(xpath = "//*[contains(text(),'Please select a specific item')]")
+    WebElement selectItemList;
+    @FindBy(xpath = "//*[contains(text(),'Please select a specific item')]/../div")
+    WebElement selectItemListButton;
+
+
+    //elements of dropdown list
+    @FindBy(xpath = "//ul[@class='chosen-results']/li[@data-option-array-index='1']")
+    WebElement itemPhysicalTherapy;
+    @FindBy(xpath = "//ul[@class='chosen-results']/li[@data-option-array-index='7']")
+    WebElement itemSpeechTherapy;
+    @FindBy(xpath = "//ul[@class='chosen-results']/li[@data-option-array-index='4']")
+    WebElement itemPsychotherapy;
+
+    //rating stars
+    @FindBy(name = "//*[@class=\"ng-isolate-scope ng-valid ng-dirty\"]/*[3]")
+    WebElement starsButton;
+
+
+    public WhatsWorksOnMainPage(WebDriver driver) {
+        super(driver);
+        PageFactory.initElements(driver, this);
+        this.PAGE_URL = "http://genefo-env.elasticbeanstalk.com/home";
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+

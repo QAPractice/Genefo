486e7c3093b32b8386a524c2f63a43d06e398390
Antoni1980 <Antishipul@yandex.ru>
2015-05-30 21:31:37 +0300

    profilePage

--- src/test/java/com/telran/LikesTest.java
+++ src/test/java/com/telran/LikesTest.java
@@ -2,16 +2,12 @@ package com.telran;
 
 import com.telran.pages.HomePage;
 import com.telran.pages.LoginPage;
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
-
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -29,6 +25,12 @@ public class LikesTest {
         wait = new WebDriverWait(driver, 5);
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         loginPage = PageFactory.initElements(driver, LoginPage.class);
+        try {
+            loginPage.openLoginPage()
+                    .waitUntilLoginPageIsLoaded();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         homePage = PageFactory.initElements(driver, HomePage.class);
 
     }

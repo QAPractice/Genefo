37527f8dedca21daae6442332595193a78923269
yacov <jakoff@gmail.com>
2015-06-05 10:51:43 +0300

    1. Added better reporting system - you will fing test report at \target\surefire-reports\html\index.html file
    2. Corrected wrong hardcoded path to Firefox binary -
    please, use standart browser invocation driver =new FirefoxDriver

--- src/test/java/com/telran/EditAccountTest.java
+++ src/test/java/com/telran/EditAccountTest.java
@@ -5,14 +5,11 @@ import com.telran.pages.LoginPage;
 import com.telran.pages.MainPage;
 import com.telran.util.TestUtils;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
 
-import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 import static org.testng.Assert.assertEquals;
@@ -41,11 +38,12 @@ public class EditAccountTest {
     public void setup(){
 
         TestUtils.addTestToLog();
-        File file = new File("C://Users//E.Frumker//AppData//Local//Mozilla Firefox//firefox.exe");
-        FirefoxBinary binary = new FirefoxBinary(file);
-        FirefoxProfile profile = new FirefoxProfile();
-        this.driver = new FirefoxDriver(binary,profile);
-        wait = new WebDriverWait(driver,5);
+        //File file = new File("C://Users//E.Frumker//AppData//Local//Mozilla Firefox//firefox.exe");
+        //FirefoxBinary binary = new FirefoxBinary(file);
+        //FirefoxProfile profile = new FirefoxProfile();
+        //this.driver = new FirefoxDriver(binary,profile);
+        this.driver = new FirefoxDriver();
+        wait = new WebDriverWait(driver, 5);
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         mainPage = PageFactory.initElements(driver,MainPage.class);
         loginPage = PageFactory.initElements(driver,LoginPage.class);

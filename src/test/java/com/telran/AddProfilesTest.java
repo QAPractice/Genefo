0bbdd1d31cdfffb6733fe3c57639728548f46698
ri-lopatina <ri-lopatina@yandex.ru>
2015-06-07 16:05:30 +0300

    After fixing

--- src/test/java/com/telran/AddProfilesTest.java
+++ src/test/java/com/telran/AddProfilesTest.java
@@ -5,17 +5,33 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
 import java.util.concurrent.TimeUnit;
-
 import static org.testng.AssertJUnit.assertTrue;
 
 /**
  * Created by Л on 5/19/2015.
  */
 public class AddProfilesTest {
+    private static String EMAIL="ri-lopatina@yandex.ru";
+    private static String PASSWORD="123456";
+    private static String FIRST_MAME="Aaa";
+    private static String LAST_NAME="Bbb";
+    private static String PATIENT_PROFILE_TYPE="2";
+    private static String PATIENT_PROFILE_TYPE_CHECK="Friend";
+    private static String GENDER="0";
+    private static String GENDER_CHECK="Male";
+    private static String CONDITION="Alstrom";
+    private static String MONTH="6";
+    private static String MONTH_CHECK="July";
+    private static String DAY="0";
+    private static String DAY_CHECK="1";
+    private static String YEAR="5";
+    private static String YEAR_CHECK="2010";
+    private static String DIAGNOSE_YEAR="1";
+    private static String DIAGNOSE_YEAR_CHECK="2014";
     public WebDriver driver;
     public WebDriverWait wait;
     MyProfilesPage myProfilesPage;
@@ -39,15 +55,15 @@ public class AddProfilesTest {
         try {
             loginPage.openLoginPage()
                     .isOnLoginPage();
-            loginPage.fillEmailField("ri-lopatina@yandex.ru")
-                    .fillPasswordField("123456")
+            loginPage.fillEmailField(EMAIL)
+                    .fillPasswordField(PASSWORD)
                     .clickOnLogin();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    @Test
+    @Test (groups = {"smoke", "positive"})
     public void AddProfileSuccess() {
         mainPage.isOnMainPage();
         mainPage.selectMyProfile();
@@ -55,83 +71,51 @@ public class AddProfilesTest {
         myProfilesPage.clickToPlus();
         profilePage.waitUntilProfilePageIsLoaded();
         profilePage.isOnProfilePage();
-        profilePage.fillProfileFirstNameField("AAA");
-        profilePage.fillProfileLastNameField("BBBB");
+        profilePage.fillProfileFirstNameField(FIRST_MAME);
+        profilePage.fillProfileLastNameField(LAST_NAME);
         String name = profilePage.getProfileName();
-        profilePage.selectProfilePatient("2");
-        profilePage.isPatientSelected("Friend");
-        profilePage.selectGender("0");
-        profilePage.isGenderSelected("Male");
-        profilePage.fillProfileConditionField("Alstrom");
+        profilePage.selectProfilePatient(PATIENT_PROFILE_TYPE);
+        profilePage.isPatientSelected(PATIENT_PROFILE_TYPE_CHECK);
+        profilePage.selectGender(GENDER);
+        profilePage.isGenderSelected(GENDER_CHECK);
+        profilePage.fillProfileConditionField(CONDITION);
         profilePage.autoFillCondition();
-        profilePage.selectMonth("6");
-        profilePage.isMonthSelected("July");
-        profilePage.selectDay("0");
-        profilePage.isDaySelected("1");
-        profilePage.selectYear("5");
-        profilePage.isYearSelected("2010");
-        profilePage.selectDiagnoseYear("1");
-        profilePage.isDiagnoseYearSelected("2014");
+        profilePage.selectMonth(MONTH);
+        profilePage.isMonthSelected(MONTH_CHECK);
+        profilePage.selectDay(DAY);
+        profilePage.isDaySelected(DAY_CHECK);
+        profilePage.selectYear(YEAR);
+        profilePage.isYearSelected(YEAR_CHECK);
+        profilePage.selectDiagnoseYear(DIAGNOSE_YEAR);
+        profilePage.isDiagnoseYearSelected(DIAGNOSE_YEAR_CHECK);
         profilePage.clickToSubmit();
         assertTrue(summaryPage.isOnSummaryPage());
         assertTrue(summaryPage.isProfileNamePresents(name));
-        summaryPage.clickOnHome();
+        summaryPage.clickOnDiscoverHome();
     }
     //  Negative tests
-    @Test      //Bug!!!
+    @Test (groups = {"smoke", "negative"})    //Bug!!!
     public void AddProfileWithoutCondition() {
-         mainPage.isOnMainPage();
-        mainPage.selectMyProfile();
-        myProfilesPage.isOnMyProfilesPage();
-        myProfilesPage.clickToPlus();
-        profilePage.waitUntilProfilePageIsLoaded();
-        profilePage.isOnProfilePage();
-        profilePage.fillProfileFirstNameField("AAAAA");
-        profilePage.fillProfileLastNameField("BBBBBB");
-        profilePage.selectProfilePatient("2");
-        profilePage.isPatientSelected("Friend");
-        profilePage.selectGender("0");
-        profilePage.isGenderSelected("Male");
-        profilePage.selectMonth("6");
-        profilePage.isMonthSelected("July");
-        profilePage.selectDay("0");
-        profilePage.isDaySelected("1");
-        profilePage.selectYear("5");
-        profilePage.isYearSelected("2010");
-        profilePage.selectDiagnoseYear("1");
-        profilePage.isDiagnoseYearSelected("2014");
-        profilePage.clickToSubmit();
-       try {
-           Thread.sleep(2000);
-       } catch (InterruptedException e) {
-           e.printStackTrace();
-       }
-       assertTrue(profilePage.isOnProfilePage());
-    }
-
-    @Test
-    public void AddProfileWithoutFirstName() {
         mainPage.isOnMainPage();
         mainPage.selectMyProfile();
         myProfilesPage.isOnMyProfilesPage();
         myProfilesPage.clickToPlus();
         profilePage.waitUntilProfilePageIsLoaded();
         profilePage.isOnProfilePage();
-        profilePage.fillProfileLastNameField("BBBBBB");
-        profilePage.selectProfilePatient("2");
-        profilePage.isPatientSelected("Friend");
-        profilePage.selectGender("0");
-        profilePage.isGenderSelected("Male");
-        profilePage.fillProfileConditionField("Alstrom");
-        profilePage.autoFillCondition();
-        profilePage.selectMonth("6");
-        profilePage.isMonthSelected("July");
-        profilePage.selectDay("0");
-        profilePage.isDaySelected("1");
-        profilePage.selectYear("5");
-        profilePage.isYearSelected("2010");
-        profilePage.selectDiagnoseYear("1");
-        profilePage.isDiagnoseYearSelected("2014");
+        profilePage.fillProfileFirstNameField(FIRST_MAME);
+        profilePage.fillProfileLastNameField(LAST_NAME);
+        profilePage.selectProfilePatient(PATIENT_PROFILE_TYPE);
+        profilePage.isPatientSelected(PATIENT_PROFILE_TYPE_CHECK);
+        profilePage.selectGender(GENDER);
+        profilePage.isGenderSelected(GENDER_CHECK);
+        profilePage.selectMonth(MONTH);
+        profilePage.isMonthSelected(MONTH_CHECK);
+        profilePage.selectDay(DAY);
+        profilePage.isDaySelected(DAY_CHECK);
+        profilePage.selectYear(YEAR);
+        profilePage.isYearSelected(YEAR_CHECK);
+        profilePage.selectDiagnoseYear(DIAGNOSE_YEAR);
+        profilePage.isDiagnoseYearSelected(DIAGNOSE_YEAR_CHECK);
         profilePage.clickToSubmit();
         try {
             Thread.sleep(2000);
@@ -140,4 +124,251 @@ public class AddProfilesTest {
         }
         assertTrue(profilePage.isOnProfilePage());
     }
+    @Test (groups = {"smoke", "negative"})
+    public void AddProfileWoutLastName() {
+        mainPage.isOnMainPage();
+        mainPage.selectMyProfile();
+        myProfilesPage.isOnMyProfilesPage();
+        myProfilesPage.clickToPlus();
+        profilePage.waitUntilProfilePageIsLoaded();
+        profilePage.isOnProfilePage();
+        profilePage.fillProfileFirstNameField(FIRST_MAME);
+        profilePage.selectProfilePatient(PATIENT_PROFILE_TYPE);
+        profilePage.isPatientSelected(PATIENT_PROFILE_TYPE_CHECK);
+        profilePage.selectGender(GENDER);
+        profilePage.isGenderSelected(GENDER_CHECK);
+        profilePage.fillProfileConditionField(CONDITION);
+        profilePage.autoFillCondition();
+        profilePage.selectMonth(MONTH);
+        profilePage.isMonthSelected(MONTH_CHECK);
+        profilePage.selectDay(DAY);
+        profilePage.isDaySelected(DAY_CHECK);
+        profilePage.selectYear(YEAR);
+        profilePage.isYearSelected(YEAR_CHECK);
+        profilePage.selectDiagnoseYear(DIAGNOSE_YEAR);
+        profilePage.isDiagnoseYearSelected(DIAGNOSE_YEAR_CHECK);
+        profilePage.clickToSubmit();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        assertTrue(profilePage.isOnProfilePage());
+    }
+    @Test (groups = {"smoke", "negative"})
+    public void AddProfileWoutFirstName() {
+        mainPage.isOnMainPage();
+        mainPage.selectMyProfile();
+        myProfilesPage.isOnMyProfilesPage();
+        myProfilesPage.clickToPlus();
+        profilePage.waitUntilProfilePageIsLoaded();
+        profilePage.isOnProfilePage();
+        profilePage.fillProfileFirstNameField(LAST_NAME);
+        profilePage.selectProfilePatient(PATIENT_PROFILE_TYPE);
+        profilePage.isPatientSelected(PATIENT_PROFILE_TYPE_CHECK);
+        profilePage.selectGender(GENDER);
+        profilePage.isGenderSelected(GENDER_CHECK);
+        profilePage.fillProfileConditionField(CONDITION);
+        profilePage.autoFillCondition();
+        profilePage.selectMonth(MONTH);
+        profilePage.isMonthSelected(MONTH_CHECK);
+        profilePage.selectDay(DAY);
+        profilePage.isDaySelected(DAY_CHECK);
+        profilePage.selectYear(YEAR);
+        profilePage.isYearSelected(YEAR_CHECK);
+        profilePage.selectDiagnoseYear(DIAGNOSE_YEAR);
+        profilePage.isDiagnoseYearSelected(DIAGNOSE_YEAR_CHECK);
+        profilePage.clickToSubmit();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        assertTrue(profilePage.isOnProfilePage());
+    }
+    @Test (groups = {"negative"})
+    public void AddProfileWoutPatientType() {
+        mainPage.isOnMainPage();
+        mainPage.selectMyProfile();
+        myProfilesPage.isOnMyProfilesPage();
+        myProfilesPage.clickToPlus();
+        profilePage.waitUntilProfilePageIsLoaded();
+        profilePage.isOnProfilePage();
+        profilePage.fillProfileFirstNameField(FIRST_MAME);
+        profilePage.fillProfileFirstNameField(LAST_NAME);
+        profilePage.selectGender(GENDER);
+        profilePage.isGenderSelected(GENDER_CHECK);
+        profilePage.fillProfileConditionField(CONDITION);
+        profilePage.autoFillCondition();
+        profilePage.selectMonth(MONTH);
+        profilePage.isMonthSelected(MONTH_CHECK);
+        profilePage.selectDay(DAY);
+        profilePage.isDaySelected(DAY_CHECK);
+        profilePage.selectYear(YEAR);
+        profilePage.isYearSelected(YEAR_CHECK);
+        profilePage.selectDiagnoseYear(DIAGNOSE_YEAR);
+        profilePage.isDiagnoseYearSelected(DIAGNOSE_YEAR_CHECK);
+        profilePage.clickToSubmit();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        assertTrue(profilePage.isOnProfilePage());
+    }
+    @Test (groups = {"negative"})
+    public void AddProfileWoutGender() {
+        mainPage.isOnMainPage();
+        mainPage.selectMyProfile();
+        myProfilesPage.isOnMyProfilesPage();
+        myProfilesPage.clickToPlus();
+        profilePage.waitUntilProfilePageIsLoaded();
+        profilePage.isOnProfilePage();
+        profilePage.fillProfileFirstNameField(FIRST_MAME);
+        profilePage.fillProfileFirstNameField(LAST_NAME);
+        profilePage.selectProfilePatient(PATIENT_PROFILE_TYPE);
+        profilePage.isPatientSelected(PATIENT_PROFILE_TYPE_CHECK);
+        profilePage.fillProfileConditionField(CONDITION);
+        profilePage.autoFillCondition();
+        profilePage.selectMonth(MONTH);
+        profilePage.isMonthSelected(MONTH_CHECK);
+        profilePage.selectDay(DAY);
+        profilePage.isDaySelected(DAY_CHECK);
+        profilePage.selectYear(YEAR);
+        profilePage.isYearSelected(YEAR_CHECK);
+        profilePage.selectDiagnoseYear(DIAGNOSE_YEAR);
+        profilePage.isDiagnoseYearSelected(DIAGNOSE_YEAR_CHECK);
+        profilePage.clickToSubmit();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        assertTrue(profilePage.isOnProfilePage());
+    }
+    @Test (groups = {"negative"})
+    public void AddProfileWoutMonth() {
+        mainPage.isOnMainPage();
+        mainPage.selectMyProfile();
+        myProfilesPage.isOnMyProfilesPage();
+        myProfilesPage.clickToPlus();
+        profilePage.waitUntilProfilePageIsLoaded();
+        profilePage.isOnProfilePage();
+        profilePage.fillProfileFirstNameField(FIRST_MAME);
+        profilePage.fillProfileFirstNameField(LAST_NAME);
+        profilePage.selectProfilePatient(PATIENT_PROFILE_TYPE);
+        profilePage.isPatientSelected(PATIENT_PROFILE_TYPE_CHECK);
+        profilePage.selectGender(GENDER);
+        profilePage.isGenderSelected(GENDER_CHECK);
+        profilePage.fillProfileConditionField(CONDITION);
+        profilePage.autoFillCondition();
+        profilePage.selectDay(DAY);
+        profilePage.isDaySelected(DAY_CHECK);
+        profilePage.selectYear(YEAR);
+        profilePage.isYearSelected(YEAR_CHECK);
+        profilePage.selectDiagnoseYear(DIAGNOSE_YEAR);
+        profilePage.isDiagnoseYearSelected(DIAGNOSE_YEAR_CHECK);
+        profilePage.clickToSubmit();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        assertTrue(profilePage.isOnProfilePage());
+    }
+    @Test (groups = {"negative"})
+    public void AddProfileWoutDay() {
+        mainPage.isOnMainPage();
+        mainPage.selectMyProfile();
+        myProfilesPage.isOnMyProfilesPage();
+        myProfilesPage.clickToPlus();
+        profilePage.waitUntilProfilePageIsLoaded();
+        profilePage.isOnProfilePage();
+        profilePage.fillProfileFirstNameField(FIRST_MAME);
+        profilePage.fillProfileFirstNameField(LAST_NAME);
+        profilePage.selectProfilePatient(PATIENT_PROFILE_TYPE);
+        profilePage.isPatientSelected(PATIENT_PROFILE_TYPE_CHECK);
+        profilePage.selectGender(GENDER);
+        profilePage.isGenderSelected(GENDER_CHECK);
+        profilePage.fillProfileConditionField(CONDITION);
+        profilePage.autoFillCondition();
+        profilePage.selectMonth(MONTH);
+        profilePage.isMonthSelected(MONTH_CHECK);
+        profilePage.selectYear(YEAR);
+        profilePage.isYearSelected(YEAR_CHECK);
+        profilePage.selectDiagnoseYear(DIAGNOSE_YEAR);
+        profilePage.isDiagnoseYearSelected(DIAGNOSE_YEAR_CHECK);
+        profilePage.clickToSubmit();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        assertTrue(profilePage.isOnProfilePage());
+    }
+    @Test (groups = {"negative"})
+    public void AddProfileWoutYear() {
+        mainPage.isOnMainPage();
+        mainPage.selectMyProfile();
+        myProfilesPage.isOnMyProfilesPage();
+        myProfilesPage.clickToPlus();
+        profilePage.waitUntilProfilePageIsLoaded();
+        profilePage.isOnProfilePage();
+        profilePage.fillProfileFirstNameField(FIRST_MAME);
+        profilePage.fillProfileFirstNameField(LAST_NAME);
+        profilePage.selectProfilePatient(PATIENT_PROFILE_TYPE);
+        profilePage.isPatientSelected(PATIENT_PROFILE_TYPE_CHECK);
+        profilePage.selectGender(GENDER);
+        profilePage.isGenderSelected(GENDER_CHECK);
+        profilePage.fillProfileConditionField(CONDITION);
+        profilePage.autoFillCondition();
+        profilePage.selectMonth(MONTH);
+        profilePage.isMonthSelected(MONTH_CHECK);
+        profilePage.selectDay(DAY);
+        profilePage.isDaySelected(DAY_CHECK);
+        profilePage.selectDiagnoseYear(DIAGNOSE_YEAR);
+        profilePage.isDiagnoseYearSelected(DIAGNOSE_YEAR_CHECK);
+        profilePage.clickToSubmit();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        assertTrue(profilePage.isOnProfilePage());
+    }
+    @Test (groups = {"negative"})
+    public void AddProfileWoutDiagnoseYear() {
+        mainPage.isOnMainPage();
+        mainPage.selectMyProfile();
+        myProfilesPage.isOnMyProfilesPage();
+        myProfilesPage.clickToPlus();
+        profilePage.waitUntilProfilePageIsLoaded();
+        profilePage.isOnProfilePage();
+        profilePage.fillProfileFirstNameField(LAST_NAME);
+        profilePage.selectProfilePatient(PATIENT_PROFILE_TYPE);
+        profilePage.isPatientSelected(PATIENT_PROFILE_TYPE_CHECK);
+        profilePage.selectGender(GENDER);
+        profilePage.isGenderSelected(GENDER_CHECK);
+        profilePage.fillProfileConditionField(CONDITION);
+        profilePage.autoFillCondition();
+        profilePage.selectMonth(MONTH);
+        profilePage.isMonthSelected(MONTH_CHECK);
+        profilePage.selectDay(DAY);
+        profilePage.isDaySelected(DAY_CHECK);
+        profilePage.selectYear(YEAR);
+        profilePage.isYearSelected(YEAR_CHECK);
+        profilePage.clickToSubmit();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        assertTrue(profilePage.isOnProfilePage());
+    }
+    @AfterClass(alwaysRun = true)
+    public void teardown() {
+        this.driver.quit();
+    }
+
+
 }

4591a15f421c6801579f41461f150c42ccfc8057
demoniskatel <demoniskatel@gmail.com>
2015-06-18 22:06:52 +0300

    AddProfilesTest optimisation

--- src/test/java/com/telran/AddProfilesTest.java
+++ src/test/java/com/telran/AddProfilesTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -24,11 +25,11 @@ public class AddProfilesTest {
     private static String PATIENT_PROFILE_TYPE_CHECK="Friend";
     private static String GENDER="0";
     private static String GENDER_CHECK="Male";
-    private static String CONDITION = "Insomnia, Fatal Familial";
+    private static String CONDITION = "Paragangliomas";
     private static String MONTH="6";
     private static String MONTH_CHECK="July";
-    private static String DAY="0";
-    private static String DAY_CHECK="1";
+    private static String DAY = "9";
+    private static String DAY_CHECK = "10";
     private static String YEAR="5";
     private static String YEAR_CHECK="2010";
     private static String DIAGNOSE_YEAR="1";
@@ -37,13 +38,13 @@ public class AddProfilesTest {
 //Constants for checking another profile test
     private static String FIRST_MAME1="Reg";
     private static String LAST_NAME1="Lop";
-    private static String PATIENT_PROFILE_TYPE_CHECK1="It's Me";
+    private static String PATIENT_PROFILE_TYPE_CHECK1 = "me";
     private static String GENDER_CHECK1="Female";
     private static String CONDITION1 = "Diarrhea";
     private static String MONTH_CHECK1 = "October";
     private static String DAY_CHECK1="17";
     private static String YEAR_CHECK1="1983";
-    private static String DIAGNOSE_YEAR_CHECK1="February 1987";
+    private static String DIAGNOSE_YEAR_CHECK1 = "1987";
 
     public WebDriver driver;
     public WebDriverWait wait;
@@ -369,7 +370,7 @@ public class AddProfilesTest {
         assertTrue("Main Page is opened", profilePage.isOnProfilePage());
     }
     //Sitechko: TCFB 2
-    @Test(groups = {"bugs", "positive"})
+    @Test(groups = {"smoke", "positive"})
     public void CheckAnotherProfile() {
         mainPage.isOnMainPage();
         mainPage.selectMyProfile();
@@ -381,30 +382,19 @@ public class AddProfilesTest {
             e.printStackTrace();
         }
         summaryPage.isOnSummaryPage();
-        assertTrue("Patient profile type is not correct", summaryPage.isRelationCorrect(PATIENT_PROFILE_TYPE_CHECK + " "));
-        assertTrue("Name is not correct", summaryPage.isNameCorrect(FIRST_MAME + " " + LAST_NAME));
-        assertTrue("Condition is not correct", summaryPage.isConditionCorrect(CONDITION + " "));
-        assertTrue("Gender is not correct", summaryPage.isGenderFieldCorrect(GENDER_CHECK + " "));
-        assertTrue("Birthday is not correct", summaryPage.isBirthdayFieldCorrect(MONTH_CHECK + " 0" + DAY_CHECK + ", " + YEAR_CHECK));
-        assertTrue("Diagnose date is not correct", summaryPage.isPatientDiagnosisDateFieldCorrect(DIAGNOSE_YEAR_CHECK));
+        assertTrue(summaryPage.areProfileFieldsCorrect(PATIENT_PROFILE_TYPE_CHECK, FIRST_MAME, LAST_NAME, CONDITION, GENDER_CHECK, MONTH_CHECK, DAY_CHECK, YEAR_CHECK, DIAGNOSE_YEAR_CHECK));
         summaryPage.clickOnFirstProfile();
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        assertTrue("Not on Summary Page", summaryPage.isOnSummaryPage());
-        assertTrue("Patient profile type1 is not correct", summaryPage.isRelationCorrect(PATIENT_PROFILE_TYPE_CHECK1 + " "));
-        assertTrue("Name1 is not correct", summaryPage.isNameCorrect(FIRST_MAME1 + " " + LAST_NAME1));
-        assertTrue("Condition1 is not correct", summaryPage.isConditionCorrect(CONDITION1 + " "));
-        assertTrue("Gender1 is not correct", summaryPage.isGenderFieldCorrect(GENDER_CHECK1 + " "));
-        assertTrue("Birthday1 is not correct", summaryPage.isBirthdayFieldCorrect(MONTH_CHECK1 + " " + DAY_CHECK1 + ", " + YEAR_CHECK1));
-        //assertTrue("Diagnose date1 is not correct", summaryPage.isPatientDiagnosisDateFieldCorrect(" " + DIAGNOSE_YEAR_CHECK1));
+        assertTrue(summaryPage.areProfileFieldsCorrect(PATIENT_PROFILE_TYPE_CHECK1, FIRST_MAME1, LAST_NAME1, CONDITION1, GENDER_CHECK1, MONTH_CHECK1, DAY_CHECK1, YEAR_CHECK1, DIAGNOSE_YEAR_CHECK1));
         summaryPage.clickOnDiscoverHome();
     }
 
-    /*@AfterClass(alwaysRun = true)
+    @AfterClass(alwaysRun = true)
     public void teardown() {
         this.driver.quit();
-    }*/
+    }
 }

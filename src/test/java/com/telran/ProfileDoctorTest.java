5ce50ba0afadebe0a2befdb795c602c5948e904a
Oleg Shtulberg <osh_il@yahoo.com>
2015-06-20 21:54:15 +0300

    fix

--- src/test/java/com/telran/ProfileDoctorTest.java
+++ src/test/java/com/telran/ProfileDoctorTest.java
@@ -1,6 +1,6 @@
 package com.telran;
 
-import com.telran.pages.ProfileDoctorPage;
+import com.telran.pages.*;
 import com.telran.util.TestUtils;
 import com.telran.util.WEB_DRIVER;
 import org.openqa.selenium.Alert;
@@ -14,8 +14,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import com.telran.pages.LoginPage;
-import com.telran.pages.MainPage;
+
 import java.util.concurrent.TimeUnit;
 
 import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
@@ -31,6 +30,9 @@ public class ProfileDoctorTest {
     public LoginPage loginPage;
     public MainPage mainPage;
     public ProfileDoctorPage profileDoctorPage;
+    public DocAcInfPage docAcInfPage;
+    public DocBasInfPage docBasInfPage;
+    public DocProfInfPage docProfInfPage;
     private boolean acceptNextAlert = true;
     public String EmailNickname; // Keeps the part of email before sign @
 
@@ -41,6 +43,9 @@ public class ProfileDoctorTest {
         loginPage = PageFactory.initElements(driver, LoginPage.class);
         mainPage = PageFactory.initElements(driver, MainPage.class);
         profileDoctorPage = PageFactory.initElements(driver, ProfileDoctorPage.class);
+        docAcInfPage = PageFactory.initElements(driver, DocAcInfPage.class);
+        docBasInfPage = PageFactory.initElements(driver, DocBasInfPage.class);
+        docProfInfPage = PageFactory.initElements(driver, DocProfInfPage.class);
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
         try {
@@ -59,6 +64,39 @@ public class ProfileDoctorTest {
         try {
             profileDoctorPage.clickOnDisYourHP();
             assertTrue("The Main Page doesn't open", mainPage.isOnMainPage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test(groups = {"smoke", "positive"})
+    public void EditAccountInformation() {
+
+        try {
+            profileDoctorPage.clickOnEditAccInf();
+            assertTrue("The Account Information Page doesn't open", docAcInfPage.isOnDocAcInfPage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test(groups = {"smoke", "positive"})
+    public void EditBasicInformation() {
+
+        try {
+            profileDoctorPage.clickOnEditBasInf();
+            assertTrue("The Basic Information Page doesn't open", docBasInfPage.isOnDocBasInfPage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test(groups = {"smoke", "positive"})
+    public void EditHealthcareProfInf() {
+
+        try {
+            profileDoctorPage.clickOnHealInf();
+            assertTrue("The Healthcare Professional Information Page doesn't open", docProfInfPage.isOnDocProfInfPage());
         } catch (Exception e) {
             e.printStackTrace();
         }
